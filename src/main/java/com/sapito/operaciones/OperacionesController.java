817f5914d86f8f36111f089d62014b865217e1df
eddy16 <edcirilo_9216@live.com.mx>
2015-02-07 02:50:31 -0600

    Templates

---
+++ src/main/java/com/sapito/operaciones/OperacionesController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sapito.operaciones;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Edgar
+ */
+@Controller
+public class OperacionesController {
+
+    @RequestMapping(value="operaciones", method=RequestMethod.GET)
+    public String indexOperaciones(Model model)
+    {
+        return "Operaciones/index";
+    }
+    
+    @RequestMapping(value="template1", method=RequestMethod.GET)
+    public String template1(Model model)
+    {
+        return "Operaciones/template1";
+    }
+    @RequestMapping(value="template2", method=RequestMethod.GET)
+    public String template2(Model model)
+    {
+        return "Operaciones/template2";
+    }
+}
+
+
