e9acf75e618a4cdd7d5206046c58ec0362e67857
Giovanni Aguirre <>
2015-02-07 01:04:52 -0600

    Commit inicial del proyecto, template basico y MainController

---
+++ src/main/java/com/sapito/controller/MainController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sapito.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author giovanni
+ */
+@Controller
+public class MainController
+{
+    
+    @RequestMapping(value="sapoindex", method=RequestMethod.GET)
+    public String index(Model model)
+    {
+        return "sapoindex";
+    }
+}
