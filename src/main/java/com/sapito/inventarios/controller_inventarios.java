f281565d73ab2af5edf6894df53a706ab502c0cc
Jorge <jovic_11@hotmail.com>
2015-02-07 02:01:02 -0600

    añadiendo paquete y controller de inventario

---
+++ src/main/java/com/sapito/inventarios/controller_inventarios.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sapito.inventarios;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Jovic
+ */
+@Controller
+
+public class controller_inventarios
+
+{
+    @RequestMapping(value="sapoindex", method=RequestMethod.GET)
+    public String index(Model model)
+    {
+        return "sapoindex";
+    }
+    
+}
