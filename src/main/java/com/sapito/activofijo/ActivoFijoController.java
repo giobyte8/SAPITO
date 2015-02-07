1dba257994b60f881758da37410786e11e9e4559
jramirez10280009 <https://jramirez10280009@bitbucket.org>
2015-02-07 03:21:51 -0600

    Se agregó controller activofijo Se agregó index activo fijo

---
+++ src/main/java/com/sapito/activofijo/ActivoFijoController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sapito.activofijo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Usuario
+ */
+@Controller
+public class ActivoFijoController {
+    @RequestMapping(value="activofijo", method=RequestMethod.GET)
+    public String index(Model model){
+        return "ActivoFijo/indexactivofijo";
+    }
+    
+}
---
+++ src/main/webapp/WEB-INF/views/ActivoFijo/indexactivofijo.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : indexactivofijo
+    Created on : Feb 7, 2015, 3:15:33 AM
+    Author     : Usuario
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello Activo!</h1>
+    </body>
+</html>
