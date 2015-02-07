efd8c32f8aa629e09adb1e8d955e58e097776a93
LAWL <LAWL@192.168.1.234>
2015-02-07 02:38:24 -0600

    agregar Direccion controller +

---
+++ src/main/java/com/sapito/direccion/DireccionController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sapito.direccion;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author LAWL
+ */
+@Controller
+public class DireccionController {
+    
+    @RequestMapping(value="direccion", method=RequestMethod.GET)
+  public String index(Model model){
+        return "Direccion/indexdireccion";
+    }
+    }
+
+
