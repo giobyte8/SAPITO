8d169a0fe12e07c122071a376bf427f28eddf00e
Jorge <jovic_11@hotmail.com>
2015-02-16 02:16:14 -0600

    Add Login to Sapito
    -Se agrego el login las contraseñas son el nombre de sus modulos en
    mayuscula

--- src/main/java/com/sapito/controller/MainController.java
+++ src/main/java/com/sapito/controller/MainController.java
@@ -18,9 +18,48 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController
 {
     
-    @RequestMapping(value="sapoindex", method=RequestMethod.GET)
-    public String index(Model model)
+    @RequestMapping(value="/", method=RequestMethod.GET)
+    public String redirect(Model model)
     {
         return "sapoindex";
+    }
+    @RequestMapping(value="loginIndex", method=RequestMethod.POST)
+    public String login(Model model,String username, String password)
+    {
+        System.out.println(username);
+        System.out.println(password);
+        
+        boolean b=true;
+        if("VENTAS".equals(username)){
+            return "redirect:ventas"; 
+        }
+        if("RH".equals(username)){
+            return "redirect:recursoshumanos"; 
+        }
+        if("INVENTARIOS".equals(username)){
+            return "redirect:inventarios"; 
+        }
+        if("CONTABILIDAD".equals(username)){
+            return "redirect:contabilidad"; 
+        }
+        if("COMPRAS".equals(username)){
+            return "redirect:compras"; 
+        }
+        if("ACTIVOFIJO".equals(username)){
+            return "redirect:activofijo"; 
+        }
+        if("DIRECCION".equals(username)){
+            return "redirect:direccion"; 
+        }
+        if("OPERACIONES".equals(username)){
+            return "redirect:operaciones"; 
+        }
+        else{
+            
+           String error = "<br><div class='alert alert-danger' role='alert'>Usuario no encontrado</div>";
+          model.addAttribute( "NotFound", error );
+          return "sapoindex";
+        }
+       
     }
 }
