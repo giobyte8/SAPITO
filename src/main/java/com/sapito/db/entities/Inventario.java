4b5f1c4cc35dad783dfe1c54bba512df497ef596
ArturoArreola <r2r_1310@hotmail.com>
2015-05-05 14:49:52 -0500

    Entities de Inventarios

---
+++ src/main/java/com/sapito/db/entities/Inventario.java
@@ -0,0 +1,208 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sapito.db.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ *
+ */
+@Entity
+@Table(name = "inventario")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Inventario.findAll", query = "SELECT p FROM Inventario p")})
+public class Inventario implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @NotNull
+    @Column(name = "idinventario")
+    private Integer idinventario;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 70)
+    @Column(name = "codigoInventario")
+    private String codigoInventario;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "tipoProducto")
+    private String tipoProducto;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "categoria")
+    private String categoria;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "cantidad")
+    private double cantidad;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "minimo")
+    private Integer minimo;
+     @Basic(optional = false)
+    @NotNull
+    @Column(name = "maximo")
+    private Integer maximo;
+      @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "ubicacion")
+    private String ubicacion;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fechaEntrada")
+    @Temporal(TemporalType.DATE)
+    private Date fechaEntrada; 
+     @Basic(optional = false)
+    @NotNull
+    @Column(name = "fechaProduccion")
+    @Temporal(TemporalType.DATE)
+    private Date fechaProduccion;
+     
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "productoIdinventario")
+    private Collection<ProductoAdquirido> productoadquiridoCollection;
+
+    public Inventario() {
+    }
+
+    public Inventario(Integer idinventario ){
+        this.idinventario = idinventario;
+    }
+
+    public Inventario(Integer idinventario, String nombre, String codigoInventario, String categoria, double cantidad, Integer minimo, Integer maximo
+            , String ubicacion, Date fechaEntrada, Date fechaProduccion) {
+        this.idinventario = idinventario;
+        this.nombre = nombre;
+        this.codigoInventario = codigoInventario;
+        this.categoria = categoria;
+        this.cantidad = cantidad;
+        this.minimo = minimo;
+        this.maximo = maximo;
+        this.ubicacion = ubicacion;
+        this.fechaEntrada = fechaEntrada;
+        this.fechaProduccion = fechaProduccion;
+    }
+
+    public Integer getIdinventario() {
+        return idinventario;
+    }
+
+    public void setIdinventario(Integer idinventario) {
+        this.idinventario = idinventario;
+    }
+
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigoInventario() {
+        return codigoInventario;
+    }
+
+    public void setCodigoInventario(String codigoInventario) {
+        this.codigoInventario = codigoInventario;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(double cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Integer getMinimo() {
+        return minimo;
+    }
+
+    public void setMinimo(Integer minimo) {
+        this.minimo = minimo;
+    }
+
+    public Integer getMaximo() {
+        return maximo;
+    }
+
+    public void setMaximo(Integer maximo) {
+        this.maximo = maximo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public Date getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(Date fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public Date getFechaProduccion() {
+        return fechaProduccion;
+    }
+
+    public void setFechaProduccion(Date fechaProduccion) {
+        this.fechaProduccion = fechaProduccion;
+    }
+
+    
+    
+    
+
+    @Override
+    public String toString() {
+        return "com.sapito.db.entities.Inventario idinventario=" + idinventario + " ]";
+    }
+    
+}
