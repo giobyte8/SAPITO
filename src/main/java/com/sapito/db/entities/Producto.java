f086bbe15edc3808cf172eb455dc66fe5e8991ba
chef <chef@DelChef>
2015-05-20 21:28:12 -0500

    Cambios a entitie Procuto

--- src/main/java/com/sapito/db/entities/Producto.java
+++ src/main/java/com/sapito/db/entities/Producto.java
@@ -40,19 +40,16 @@ public class Producto implements Serializable {
 
     @NotNull
     @Column(name = "NOMBRE")
-    @Pattern(regexp = RExp.letrasAcentuadasPuntos, message = RExpErrors.letrasAcentuadasPuntos)
     @Size(min = 2, max = 100, message = "Debe tener entre 2 y 100 caracteres")
     private String nombreProducto;
 
     @NotNull
     @Column(name = "DESCRIPCION")
-    @Pattern(regexp = RExp.letrasAcentuadasPuntos, message = RExpErrors.letrasAcentuadasPuntos)
     @Size(min = 2, max = 100, message = "Debe tener entre 2 y 100 caracteres")
     private String descripcion;
 
     @NotNull
     @Column(name = "MARCA")
-    @Pattern(regexp = RExp.letrasBasicasDigitos, message = RExpErrors.letrasBasicasDigitos)
     @Size(min = 2, max = 10, message = "Debe tener entre 2 y 10 caracteres")
     private String marca;
 
